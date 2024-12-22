05ee402fd77aed54834e066e1ba09b17640d0c86
Niel22 <niel2264@gmail.com>
2024-12-22 19:38:31 +0100

    Created and Read From User Table

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/users");
+const multer = require("multer");
 
-router.get('/', (req, res) => {
-    res.render('index', {title: "Home Page"});
+// Image upload
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
 });
 
-router.get('/add', (req, res) => {
-    res.render('add_users', {title: "Add User"});
+var upload = multer({
+  storage: storage,
+}).single("image");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.render("index", {
+      title: "Home Page",
+      users: users,
+    });
+  } catch (error) {
+    res.json({ message: error.message, type: "danger" });
+  }
+});
+
+router.get("/add", (req, res) => {
+  res.render("add_users", { title: "Add User" });
+});
+
+router.post("/add", upload, async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      image: req.file.filename,
+    });
+    await user.save();
+
+    req.session.message = {
+      type: "success",
+      message: "User created Successfully",
+    };
+
+    res.redirect("/");
+  } catch (error) {
+    res.json({ message: error.message, type: "danger" });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static("uploads"));
+
 app.set('view engine', 'ejs');
 
 
